4b622c5823cf8102b2288167edb941846db0181c
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-01-20 10:17:06 +0000

    Add strings

--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,12 @@ fn main() {
     println!("p = {}", p);
     let arr: [usize; 3] = [2, 3, 5];
     assert_eq!(arr[1], 3);
+    assert_eq!(arr, [2, 3, 5]);
     assert_eq!(&arr, &[2, 3, 5]);
     assert_eq!(&arr[0..1], &[2]);
     assert_eq!(&arr[1..], &[3, 5]);
+    let s: &str = "hello";
+    assert_eq!(&s[..2], "he");
+    let ss: String = String::from("world");
+    assert_eq!(&ss[..2], "wo");
 }
